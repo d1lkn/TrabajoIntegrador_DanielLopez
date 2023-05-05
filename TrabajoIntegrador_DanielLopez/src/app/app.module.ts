b96debae6c90a43c9e5380ff533223e9c85de0ab
Daniel Lopez <daanilopez123@gmail.com>
2023-05-05 19:54:56 -0300

    reinclusion bootstrap, edicion banner

--- TrabajoIntegrador_DanielLopez/src/app/app.module.ts
+++ TrabajoIntegrador_DanielLopez/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
-import { RsocialComponent } from './components/rsocial/rsocial.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,7 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgProgComponent,
-    RsocialComponent,
+
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
