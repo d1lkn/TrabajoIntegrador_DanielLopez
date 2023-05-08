47ad67ba20cc63cedfbc76f6f813877373f1894d
Daniel Lopez <daanilopez123@gmail.com>
2023-05-07 23:38:08 -0300

    controller, service, repository, entity, interface

---
+++ TrabajoIntegrador_DanielLopez/BackEnd/src/main/java/com/portfolio/apdani/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.apdani.Repository;
+
+import com.portfolio.apdani.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
