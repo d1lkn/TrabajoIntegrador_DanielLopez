8566301de5bbb90bbdc5f44bda4ba2d947eeb5c6
Daniel Lopez <daanilopez123@gmail.com>
2023-05-11 20:07:21 -0300

    banckedn 11/5

---
+++ TrabajoIntegrador_DanielLopez/BackEnd/daniap/daniap/src/main/java/com/portfolio/daniap/Security/Entity/Rol.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.daniap.Security.Entity;
+
+import com.portfolio.daniap.Security.Enums.RolNombre;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+    //CONSTRUCTOR
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    //getter y setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    
+    
+}
--- TrabajoIntegrador_DanielLopez/BackEnd/daniap/daniap/src/main/java/com/portfolio/daniap/Service/ImpPersonaService.java
+++ TrabajoIntegrador_DanielLopez/BackEnd/daniap/daniap/src/main/java/com/portfolio/daniap/Service/ImpPersonaService.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 
 public class ImpPersonaService  implements IPersonaService {
