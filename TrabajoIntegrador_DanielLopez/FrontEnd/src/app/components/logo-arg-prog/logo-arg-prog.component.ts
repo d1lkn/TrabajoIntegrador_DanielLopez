aedf6381c445858387186c1bec03c7de9b037f7c
Daniel Lopez <daanilopez123@gmail.com>
2023-05-12 00:08:50 -0300

    back,login

--- TrabajoIntegrador_DanielLopez/FrontEnd/src/app/components/logo-arg-prog/logo-arg-prog.component.ts
+++ TrabajoIntegrador_DanielLopez/FrontEnd/src/app/components/logo-arg-prog/logo-arg-prog.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-arg-prog',
   templateUrl: './logo-arg-prog.component.html',
   styleUrls: ['./logo-arg-prog.component.css']
 })
-export class LogoArgProgComponent {
+export class LogoArgProgComponent implements OnInit{
+
+  constructor(private router:Router) {}
+  ngOnInit(): void {
+    
+  }
+login(){
+this.router.navigate(['/login']);
+
+}
 
 }
